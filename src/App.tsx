bd7446d66f0b2c48cb491e9482cb32f52a24694d
Daniil Rodionov <zenitpv@icloud.com>
2023-11-18 12:18:53 +0300

    react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
-import "./App.css"
+import { Route, Routes } from "react-router-dom"
+import { Home } from "./pages/Home"
 
 function App() {
     return (
-        <>
-            <h1>hello world</h1>
-        </>
+        <Routes>
+            <Route path="/" element={<Home />} />
+        </Routes>
     )
 }
 
-export default App
+export { App }
--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,16 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
-import App from "./App.tsx"
 import { Provider } from "react-redux"
 import { store } from "./redux/store.ts"
+import { BrowserRouter } from "react-router-dom"
+import { App } from "./App.tsx"
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
     <React.StrictMode>
         <Provider store={store}>
-            <App />
+            <BrowserRouter>
+                <App />
+            </BrowserRouter>
         </Provider>
     </React.StrictMode>
 )
