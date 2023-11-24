ed65fdce605f324481827c26c24f19fb59d5da19
Daniil Rodionov <zenitpv@icloud.com>
2023-11-24 03:21:03 +0300

    adding query by id and extra info page

--- src/components/FilmItem.tsx
+++ src/components/FilmItem.tsx
@@ -6,15 +6,48 @@ interface FilmItemProps {
 }
 const FilmItem: FC<FilmItemProps> = ({ film }) => {
     return (
-        <a
-            href="#"
-            className="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100"
-        >
-            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900">
-                {film.Title}, {film.Year}
-            </h5>
-            <div className="font-normal text-gray-700">{film.Poster}</div>
-        </a>
+        <div className="flex-column justify-evenly max-w-sm bg-white border border-gray-200 rounded-lg shadow">
+            <div>
+                <img
+                    className="rounded-t h-72 w-full object-cover"
+                    src={film.Poster}
+                    alt=""
+                />
+            </div>
+            <div className="p-5">
+                <div className="h-40">
+                    <h5 className="mb-2 text-xl font-bold tracking-tight text-gray-900">
+                        {film.Title}
+                    </h5>
+                    <p className="mb-3 font-normal text-gray-700 ">
+                        {film.Year}
+                    </p>
+                </div>
+                <div>
+                    <a
+                        href={`/:${film.imdbID}`}
+                        className="inline-flex items-center px-4 py-4 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300   "
+                    >
+                        Read more
+                        <svg
+                            className="rtl:rotate-180 w-3.5 h-3.5 ms-2"
+                            aria-hidden="true"
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 14 10"
+                        >
+                            <path
+                                stroke="currentColor"
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                strokeWidth="2"
+                                d="M1 5h12m0 0L9 1m4 4L9 9"
+                            />
+                        </svg>
+                    </a>
+                </div>
+            </div>
+        </div>
     );
 };
 
--- src/components/FilmsList.tsx
+++ src/components/FilmsList.tsx
@@ -1,23 +1,25 @@
 import { IMovieInfo } from "../models/IMovie";
+import { Loader } from "../pages/Loader";
 import { useGetFilmsQuery } from "../services/FilmService";
 import { FilmItem } from "./FilmItem";
 
 function FilmsList() {
-    const { data } = useGetFilmsQuery("");
+    const { data, isLoading } = useGetFilmsQuery("");
     if (data) {
         const movies = data.Search;
         console.log(movies);
     }
 
+    if (isLoading) {
+        return <Loader />;
+    }
+
     return (
-        <div>
-            <div>
+        <div className="bg-gray-100  py-10 px-12">
+            <div className="grid grid-cols-2 md:grid-cols-4 gap-4">
                 {data?.Search.map((film) => {
                     return (
-                        <FilmItem
-                            key={film.imdbID}
-                            film={film as unknown as IMovieInfo}
-                        />
+                        <FilmItem key={film.imdbID} film={film as IMovieInfo} />
                     );
                 })}
             </div>
