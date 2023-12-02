ff5cd94281676409a58a1b19e610db7ff8b1b988
Даниил Родионов <33860913+qui-qui-qui@users.noreply.github.com>
2023-12-02 17:43:32 +0300

    Delete src/hooks/useFavoritesCheck.ts

--- src/hooks/useFavoritesCheck.ts
+++
@@ -1,8 +0,0 @@
-import { useSelector } from "react-redux";
-import { getFavorites } from "../redux/slices/favoritesSlice";
-
-export const useFavoritesCheck = (id: string | undefined): boolean => {
-    const favorites = useSelector(getFavorites);
-
-    return !!favorites.find((item) => item.imdbID === id);
-};
